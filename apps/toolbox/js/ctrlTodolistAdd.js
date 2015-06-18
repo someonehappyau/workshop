36e1bbba24e6246eb0df54b4741973b96c4f7902
someonehappy <someonehappyau@gmail.com>
2015-06-19 01:22:52 +1000

    todo add updating

--- apps/toolbox/js/ctrlTodolistAdd.js
+++ apps/toolbox/js/ctrlTodolistAdd.js
@@ -3,6 +3,10 @@
 todolistControllers.controller('TodolistAddCtrl',
 	['$scope','TDTodoSvc',
 	function($scope,TDTodoSvc){
+		$scope.todo={
+			dueDate:''
+		};
+
 		$scope.submitForm=function(){
 			var svcTDTodo=new TDTodoSvc();
 			$scope.todo={};
@@ -13,4 +17,23 @@ todolistControllers.controller('TodolistAddCtrl',
 				console.log('submit');
 			});
 		};
+
+		$scope.today=function(){
+			$scope.dt=new Date();
+			$scope.dt=$scope.dt.setDate($scope.dt.getDate()+8);
+			$scope.todo.dueDate=$scope.dt;
+		};
+
+		$scope.today();
+
+		$scope.minDate=new Date();
+		$scope.minDate=$scope.minDate.setDate($scope.minDate.getDate()+1);
+
+		$scope.open = function($event) {
+			$event.preventDefault();
+			$event.stopPropagation();
+
+			$scope.opened = true;
+		};
+
 	}]);
