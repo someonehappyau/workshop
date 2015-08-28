157ea5c3f27e374384fc30a0169e39e3b4615218
someonehappy <someonehappyau@gmail.com>
2015-08-28 17:37:00 +1000

    mco detail image uploading updating

--- apps/mc/js/app.js
+++ apps/mc/js/app.js
@@ -9,7 +9,8 @@ var mcApp=angular.module('mcApp',[
 	'ngSanitize',
 	'ngAnimate',
 	'toaster',
-	'ui.sortable'
+	'ui.sortable',
+	'angularFileUpload'
 ]);
 
 var mcServices=angular.module('mcServices',['ngResource']);
@@ -123,92 +124,142 @@ mcApp.factory('AuthService', function (UserSvc, Session, $cookies,$q,$location) 
 	return authService;
 	});
 
-	mcApp.config(['$routeProvider',
-		function($routeProvider){
-			$routeProvider.
-				when('/mc',{
-					templateUrl:'partials/mc.html',
-					controller:'MCCtrl'
-				}).
-				when('/mcolist',{
-					templateUrl:'partials/mcolist.html',
-					controller:'MCOriginCtrl',
-					resolve:{
-						validation: ['AuthService','$location',function(AuthService,$location){
-											AuthService.authenticate().then(function(){
-											},
-											function(){
-												$location.path('/mc');
-											});
-									}]
-					}
-				}).
-				when('/mcoDetail/:id',{
-					templateUrl:'partials/mcoDetail.html',
-					controller:'MCODetailCtrl',
-					resolve:{
-						validation: ['AuthService','$location',function(AuthService,$location){
-											AuthService.authenticate().then(function(){
-											},
-											function(){
-												$location.path('/mc');
-											});
-									}]
-					}
-				}).
-				when('/edittodo/:id',{
-					templateUrl:'partials/todolist_add.html',
-					controller:'TodolistAddCtrl',
-					resolve:{
-						validation: ['AuthService','$location',function(AuthService,$location){
-											AuthService.authenticate().then(function(){
-											},
-											function(){
-												$location.path('/toolbox');
-											});
-									}],
-						isEdit: function(){
-									return true;
-								}
-					}
-				}).
-				when('/mc/config/:typeName',{
-					templateUrl:'partials/TDConfig.html',
-					controller:'TDTypeCtrl',
-					resolve:{
-						validation: function($route,AuthService,$location){
-							AuthService.authenticate().then(function(){
-								console.log('authenticated');
-								var typeName=$route.current.params.typeName; 
-								if (typeName==='MCOrgState' || 
-									typeName==='MCMaker' ||
-									typeName==='MCModelType' ||
-									typeName==='MCEngFormat' ||	
-									typeName==='MCEngMount' ||	
-									typeName==='MCEngType' ||	
-									typeName==='MCEngCamType' ||	
-									typeName==='MCEngCooling' ||	
-									typeName==='MCEngLubrication' ||	
-									typeName==='MCEngInductionType' ||	
-									typeName==='MCEngClutchOperation' ||	
-									typeName==='MCEngClutchType' ||	
-									typeName==='MCEngIgnition' ||	
-									typeName==='MCEngStarting'	
-								){
-							    	}
-								else{
-									$location.path('/mc');
-								}
-							},
-							function(){
+mcApp.config(['$routeProvider',
+	function($routeProvider){
+		$routeProvider.
+			when('/mc',{
+				templateUrl:'partials/mc.html',
+				controller:'MCCtrl'
+			}).
+			when('/mcolist',{
+				templateUrl:'partials/mcolist.html',
+				controller:'MCOriginCtrl',
+				resolve:{
+					validation: ['AuthService','$location',function(AuthService,$location){
+										AuthService.authenticate().then(function(){
+										},
+										function(){
+											$location.path('/mc');
+										});
+								}]
+				}
+			}).
+			when('/mcoDetail/:id',{
+				templateUrl:'partials/mcoDetail.html',
+				controller:'MCODetailCtrl',
+				resolve:{
+					validation: ['AuthService','$location',function(AuthService,$location){
+										AuthService.authenticate().then(function(){
+										},
+										function(){
+											$location.path('/mc');
+										});
+								}]
+				}
+			}).
+			when('/edittodo/:id',{
+				templateUrl:'partials/todolist_add.html',
+				controller:'TodolistAddCtrl',
+				resolve:{
+					validation: ['AuthService','$location',function(AuthService,$location){
+										AuthService.authenticate().then(function(){
+										},
+										function(){
+											$location.path('/toolbox');
+										});
+								}],
+					isEdit: function(){
+								return true;
+							}
+				}
+			}).
+			when('/mc/config/:typeName',{
+				templateUrl:'partials/TDConfig.html',
+				controller:'TDTypeCtrl',
+				resolve:{
+					validation: function($route,AuthService,$location){
+						AuthService.authenticate().then(function(){
+							console.log('authenticated');
+							var typeName=$route.current.params.typeName; 
+							if (typeName==='MCOrgState' || 
+								typeName==='MCMaker' ||
+								typeName==='MCModelType' ||
+								typeName==='MCEngFormat' ||	
+								typeName==='MCEngMount' ||	
+								typeName==='MCEngType' ||	
+								typeName==='MCEngCamType' ||	
+								typeName==='MCEngCooling' ||	
+								typeName==='MCEngLubrication' ||	
+								typeName==='MCEngInductionType' ||	
+								typeName==='MCEngClutchOperation' ||	
+								typeName==='MCEngClutchType' ||	
+								typeName==='MCEngIgnition' ||	
+								typeName==='MCEngStarting'	
+							){
+							}
+							else{
 								$location.path('/mc');
-							});
-						}
+							}
+						},
+						function(){
+							$location.path('/mc');
+						});
 					}
-								
-				}).
-				otherwise({
-					redirectTo:'/mc'
-				});
-		}]);
+				}
+							
+			}).
+			otherwise({
+				redirectTo:'/mc'
+			});
+	}]);
 
+mcApp
+    /**
+    * The ng-thumb directive
+    * @author: nerv
+    * @version: 0.1.2, 2014-01-09
+    */
+    .directive('ngThumb', ['$window', function($window) {
+        var helper = {
+            support: !!($window.FileReader && $window.CanvasRenderingContext2D),
+            isFile: function(item) {
+                return angular.isObject(item) && item instanceof $window.File;
+            },
+            isImage: function(file) {
+                var type =  '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        };
+
+        return {
+            restrict: 'A',
+            template: '<canvas/>',
+            link: function(scope, element, attributes) {
+                if (!helper.support) return;
+
+                var params = scope.$eval(attributes.ngThumb);
+
+                if (!helper.isFile(params.file)) return;
+                if (!helper.isImage(params.file)) return;
+
+                var canvas = element.find('canvas');
+                var reader = new FileReader();
+
+                reader.onload = onLoadFile;
+                reader.readAsDataURL(params.file);
+
+                function onLoadFile(event) {
+                    var img = new Image();
+                    img.onload = onLoadImage;
+                    img.src = event.target.result;
+                }
+
+                function onLoadImage() {
+                    var width = params.width || this.width / this.height * params.height;
+                    var height = params.height || this.height / this.width * params.width;
+                    canvas.attr({ width: width, height: height });
+                    canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+                }
+            }
+        };
+    }]);
--- apps/mc/js/ctrlMCODetail.js
+++ apps/mc/js/ctrlMCODetail.js
@@ -1,7 +1,7 @@
 'use strict';
 
-mcControllers.controller('MCODetailCtrl',['$scope','MCOriginSvc','$routeParams','MCModelSvc','TDTypeSvc','$parse','toaster','MCEngineSvc','MCPicSvc',
-		function($scope,MCOriginSvc,$routeParams,MCModelSvc,TDTypeSvc,$parse,toaster,MCEngineSvc,MCPicSvc){
+mcControllers.controller('MCODetailCtrl',['$scope','MCOriginSvc','$routeParams','MCModelSvc','TDTypeSvc','$parse','toaster','MCEngineSvc','MCPicSvc','FileUploader',
+		function($scope,MCOriginSvc,$routeParams,MCModelSvc,TDTypeSvc,$parse,toaster,MCEngineSvc,MCPicSvc,FileUploader){
 			$scope.addAlert=function(type,msg){
 				toaster.pop(type,null,msg);
 			};
@@ -334,5 +334,57 @@ mcControllers.controller('MCODetailCtrl',['$scope','MCOriginSvc','$routeParams',
 
 			$scope.reloadTypes();
 
+			var uploader = $scope.uploader = new FileUploader({
+			    url: 'upload.php'
+			});
+
+			// FILTERS
+
+			uploader.filters.push({
+			    name: 'imageFilter',
+			    fn: function(item /*{File|FileLikeObject}*/, options) {
+				var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+				return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+			    }
+			});
+
+			// CALLBACKS
+
+			uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+			    console.info('onWhenAddingFileFailed', item, filter, options);
+			};
+			uploader.onAfterAddingFile = function(fileItem) {
+			    console.info('onAfterAddingFile', fileItem);
+			};
+			uploader.onAfterAddingAll = function(addedFileItems) {
+			    console.info('onAfterAddingAll', addedFileItems);
+			};
+			uploader.onBeforeUploadItem = function(item) {
+			    console.info('onBeforeUploadItem', item);
+			};
+			uploader.onProgressItem = function(fileItem, progress) {
+			    console.info('onProgressItem', fileItem, progress);
+			};
+			uploader.onProgressAll = function(progress) {
+			    console.info('onProgressAll', progress);
+			};
+			uploader.onSuccessItem = function(fileItem, response, status, headers) {
+			    console.info('onSuccessItem', fileItem, response, status, headers);
+			};
+			uploader.onErrorItem = function(fileItem, response, status, headers) {
+			    console.info('onErrorItem', fileItem, response, status, headers);
+			};
+			uploader.onCancelItem = function(fileItem, response, status, headers) {
+			    console.info('onCancelItem', fileItem, response, status, headers);
+			};
+			uploader.onCompleteItem = function(fileItem, response, status, headers) {
+			    console.info('onCompleteItem', fileItem, response, status, headers);
+			};
+			uploader.onCompleteAll = function() {
+			    console.info('onCompleteAll');
+			};
+
+			console.info('uploader', uploader);
+			
 		}]);
 
